cce9ee08707549cb8fa6d90b08b168f985986dbf
luyiyun <18845728185@163.com>
2020-01-04 10:59:22 +0800

    md document

--- src/parse.R
+++ src/parse.R
@@ -1,10 +1,10 @@
 library(rmarkdown)
 
 sources <- c("readme.Rmd", "MonteCarlo.Rmd")
-src_path <- file.path(getwd(), "src")
-output_path <- getwd()
+src_path <- "E:/R/Learning/MCMC/src"
+output_path <- "E:/R/Learning/MCMC"
 
 for (s in sources) {
   render(file.path(src_path, s), output_dir = output_path,
-         output_format = github_document(toc = TRUE, toc_depth = 2, html_preview = FALSE))
+         output_format = md_document(toc = TRUE, toc_depth = 2))
 }
