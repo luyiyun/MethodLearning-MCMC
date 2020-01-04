4e349f2496fd313bd619860f0df52d987b447990
luyiyun <18845728185@163.com>
2020-01-04 12:20:56 +0800

    two backslash test

--- src/parse.R
+++ src/parse.R
@@ -6,5 +6,7 @@ output_path <- "E:/R/Learning/MCMC"
 
 for (s in sources) {
   render(file.path(src_path, s), output_dir = output_path,
-         output_format = github_document(toc = TRUE, toc_depth = 2, html_preview = FALSE))
+         output_format = github_document(toc = TRUE, toc_depth = 2,
+                                         html_preview = FALSE,
+                                         md_extensions = "-tex_math_single_backslash"))
 }
